35086ded13781fe4a2928b296efad1ce96a5f362
reztechcode <irezabdullah@rezweb.my.id>
2024-03-28 10:24:09 +0700

    add base page

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     # return "<p>Hello, World!</p>"
-     return render_template('home.html')
+     return render_template('home.html',title='Home')
 
 
 @app.route('/detail/<initial>')
 def show_initial(initial):
     # show the user profile for that user
     # return f'Parameter dari url: {initial}'
-      return render_template('test.html',initial = initial)
+      return render_template('test.html',title='Detail Page',initial = initial)
      
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('notfound.html'), 404
+    return render_template('notfound.html',title='Notfound'), 404
